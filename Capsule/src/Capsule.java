c8fa470456512b9412fc746f2849673e8cec5a20
kein <kein10119810@gmail.com>
2025-03-01 15:33:28 +0900

    setage

--- Capsule/src/Capsule.java
+++ Capsule/src/Capsule.java
@@ -5,6 +5,14 @@ public class Capsule {
 		Person taro=new Person("山田太郎", 20);
 		System.out.println(taro.getName());
 
+		taro.setName("花子");
+		System.out.println(taro.getName());
+
+		System.out.println(taro.getAge());
+
+		taro.setAge(30);
+		System.out.println(taro.getAge());
+
 	}
 
 }
