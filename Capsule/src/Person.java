913d6d0770ce7ee3f28a7fe548057aebd81d9170
kein <kein10119810@gmail.com>
2025-03-01 15:19:35 +0900

    getname

--- Capsule/src/Person.java
+++ Capsule/src/Person.java
@@ -1,9 +1,20 @@
 
 public class Person {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public String name=null;
+	public int age=0;
 
+	public Person(String name, int age){
+		this.name=name;
+		this.age=age;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public void setName(String name){
+		this.name=name;
 	}
 
 }
